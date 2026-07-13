5ebe3bda536ee10ad8794ce1bc0374a7f1aaef23
Chandrika Nandehariya <chandrikanandesariya018@gmail.com>
2026-07-13 22:12:36 +0530

    Optimize RAG latency and upgrade to fast Groq model

--- retriever.py
+++ retriever.py
@@ -1,38 +1,23 @@
 from vector_store import get_vector_store
-from langchain_classic.retrievers import ContextualCompressionRetriever
-from langchain_classic.retrievers.document_compressors import LLMChainExtractor
-from langchain_groq import ChatGroq
 import os
+
 def get_retriever(video_url: str):
 
     vector_db = get_vector_store(video_url)
-
-    llm = ChatGroq(
-        model="openai/gpt-oss-20b",
-        groq_api_key=os.getenv("GROQ_API_KEY"),
-        temperature=0.3,
-        streaming=True
-    )
-
     if vector_db is None:
         return None
 
-    compressor = LLMChainExtractor.from_llm(llm)
 
     retriever = vector_db.as_retriever(
-    search_type="mmr",
-    search_kwargs={
-        "k": 5,
-        "fetch_k": 20,
-        "lambda_mult": 0.7
-    }
-)
+       search_type="mmr",
+       search_kwargs={
+         "k": 3,
+         "fetch_k": 20,
+         "lambda_mult": 0.7
+       }
+    )
 
-    compression_retriever = ContextualCompressionRetriever(
-        base_compressor=compressor,
-        base_retriever=retriever
-)
-    return compression_retriever
+    return retriever
 
 
 
