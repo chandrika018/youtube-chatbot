dc6baf5242893adc49e2902c69dc015145d38c4b
Chandrika Nandehariya <chandrikanandesariya018@gmail.com>
2026-07-13 17:57:37 +0530

    Improve retriever logic and optimize vector store implementation

--- vector_store.py
+++ vector_store.py
@@ -1,7 +1,7 @@
 import os
 
 from embedding import get_documents, get_embedding_model
-from langchain_chroma import Chroma
+from langchain_community.vectorstores import FAISS
 from transcript import extract_video_id
 
 
@@ -13,28 +13,36 @@ def get_vector_store(video_url: str):
 
     embedding_model = get_embedding_model()
 
-    # Already exists
+    # -----------------------------
+    # Load Existing FAISS Index
+    # -----------------------------
     if os.path.exists(persist_dir):
-        print("Loading Existing Chroma...")
+        print("Loading Existing FAISS...")
 
-        return Chroma(
-            persist_directory=persist_dir,
-            embedding_function=embedding_model,
-            collection_name=video_id
+        return FAISS.load_local(
+            folder_path=persist_dir,
+            embeddings=embedding_model,
+            allow_dangerous_deserialization=True
         )
 
-    print("Creating New Chroma...")
+    print("Creating New FAISS Index...")
 
     docs = get_documents(video_url)
 
     if docs is None:
         return None
 
-    vector_store = Chroma.from_documents(
+    # -----------------------------
+    # Create FAISS Index
+    # -----------------------------
+    vector_store = FAISS.from_documents(
         documents=docs,
-        embedding=embedding_model,
-        persist_directory=persist_dir,
-        collection_name=video_id
+        embedding=embedding_model
     )
+
+    # -----------------------------
+    # Save Index
+    # -----------------------------
+    vector_store.save_local(persist_dir)
 
     return vector_store
